d9f484bcdebbdbc265a56a5721d66f4a7ce4da12
slifer2015 <dara51php@gmail.com>
2016-02-14 01:34:43 -0800

    adasd
    asdas

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -1,3 +1,48 @@
 @extends('admin.layout')
 @section('content')
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    مقاله جدید
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            {!! Form::open(['route'=>'admin.article.store','method'=>'post', 'enctype'=>'multipart/form-data']) !!}
+                            <div class="form-group">
+                                <label for="title">عنوان</label>
+                                <input id="title" name="title" class="form-control"
+                                       placeholder="عنوان را وارد نمایید ...">
+                            </div>
+                            <div class="form-group">
+                                <label for="image">درج تصویر</label>
+                                <input class="form-control" id="image" name="image" type="file">
+                            </div>
+                            {{--summernote--}}
+                            {!! Form::textarea('content', null, ['class'=>'form-control article_summernote', 'rows'=>'10']) !!}
+                            <div class="form-group">
+                                <label>وضعیت انتشار</label>
+                                {!! Form::select('published', [1=>'منتشر شود', 0=>'منتشر نشود'], 1, ['class'=>'form-control select-status', 'placeholder'=>'']) !!}
+                            </div>
+                            <div class="form-group">
+                                <label>برچسب ها</label>
+                                {!! Form::select('tags[]', [], null, ['id'=>'tags_select','class'=>'form-control','multiple', 'placeholder'=>'']) !!}
+                            </div>
+                            <button type="submit" class="btn btn-success">ارسال</button>
+
+                            </form>
+                        </div>
+
+                    </div>
+                    <!-- /.row (nested) -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
+            <!-- /.panel -->
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+@endsection
+@section('script')
 @endsection
